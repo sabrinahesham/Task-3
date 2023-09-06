1384036fc85baca35348ba6ab08cdd5db2c1d4c1
sabrina <sabrinahesham578@gmail.com>
2023-09-06 22:49:08 +0300

    taskkkkk

--- 01.php
+++ 01.php
@@ -3,11 +3,12 @@
  $a=4;
  echo "i have ++ $a books";
  echo '<br>';
- echo "كنتي ناسيه تفتحي اكزامب وتشغليه وكنتي ناسيه السيمي كولن ف السطرين";
 
 
 
-
+//1- git add .
+//2- git commit -m "comment what you have updated"
+//3 -git push -u origin main 
 
 
 
